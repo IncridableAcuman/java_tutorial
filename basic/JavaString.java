e6580bb08a55b906689ef345fec4d64ba6182139
Izzatbek <abdusharipovizzat03@gmail.com>
2026-05-01 13:30:12 +0500

    I completed String in Java

--- basic/JavaString.java
+++ basic/JavaString.java
@@ -1,6 +1,6 @@
 
 // Strings in Java
-
+import  java.util.*;
 public class JavaString {
     public static void main(String[] args) {
         /*
@@ -26,11 +26,15 @@ public class JavaString {
             Satrni berilgan belgiga qarab ikkiga bo‘ling va ikkala qismini alohida chiqarish.
         * */
         String username = " Incridable ";
+//        string of size
         System.out.println(username.length());
+//        to upper case
         System.out.println(username.toUpperCase());
+//        to lower case
         System.out.println(username.toLowerCase());
+//        remove space of string
         System.out.println(username.trim());
-
+// counting some character of string
         char character = 'a';
         int count = 0;
         for (int i=0;i<username.length();i++){
@@ -44,5 +48,51 @@ public class JavaString {
 
         long  icount = username.chars().filter(c->c == searchChareacter).count();
         System.out.println(icount);
+//      reverse string
+        for (int i=0;i<username.length();i++){
+            System.out.print(username.charAt(username.length()- i - 1));
+        }
+        System.out.println();
+// containing something word of string
+        String text = "Assalomu Aleykum barchaga sog'liq omonlik tilayman.";
+        String text2 = text;
+        String word = "al";
+        System.out.println(text.toLowerCase().contains(word));
+//        substring of string
+        System.out.println(text2.substring(0,5));
+//       ending with something word
+        System.out.println(text2.endsWith("tilayman."));
+//        starting with something word
+        System.out.println(text2.startsWith("Assalomu"));
+//        removing space with replace method
+        System.out.println(text2.replace(" ",""));
+//        spling with coma and pushing to array
+        System.out.println(Arrays.toString(text.replace(" ",",").split(",")));
+//      is equal two string
+        System.out.println(text.equals(text2));
+//        is equal ignore case two string
+        System.out.println(text.equalsIgnoreCase(text2));
+//        finding first  index of character
+        System.out.println(text.indexOf(character));
+//        finding last index of character
+        System.out.println(text.lastIndexOf(character));
+//       string to characters array
+        System.out.println(text.toCharArray());
+//        replaceAll()
+        String matn = "qwerty1234";
+        System.out.println(matn.replaceAll("[0-9]",""));
+//        Checking string to only word
+        String content = "qwerty";
+        String regex = "[A-Za-z]+";
+        System.out.println(content.matches(regex));
+//      ikkiga bo'lish
+        String fruits = "apple,banana,orage,peach";
+        String symbol = ",";
+        String [] qism = fruits.split(symbol,2);
+
+        if (qism.length >=2){
+            System.out.println("First: " + qism[0]);
+            System.out.println("Second: " + qism[1]);
+        }
     }
 }
